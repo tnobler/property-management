a28493091308413b21934aedd02aebb4308000ba
Tanner Noble <tannernoble@gmail.com>
2018-10-10 23:52:36 -0500

    tabnav grid styling again

--- src/components/tabNav.js
+++ src/components/tabNav.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react';
 
 class TabNav extends Component {
   render() {
-    return(
+    return (
       <div className='tab-nav'>
-      
-    </div>
+        <div className='tab-nav__tabs'>
+          {
+            this.props.tabs.map((tab, index) => {
+              return <a className='tab-nav__tab'>{tab.title}</a>
+            })
+          }
+        </div>
+        <div>requests for newsletters content goes here</div>
+
+      </div>
     )
   }
 }
