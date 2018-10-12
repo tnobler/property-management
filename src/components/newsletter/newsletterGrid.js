33c5f4c803279ff4230afc273266cfda3d50b5af
Tanner Noble <tannernoble@gmail.com>
2018-10-12 10:24:27 -0500

    built the newsletter grid component and grid

---
+++ src/components/newsletter/newsletterGrid.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NewsletterGrid extends Component {
+  render () {
+    return (
+      <div className='newsletter-grid'>
+         al;skdfj;alskjdf
+      </div>
+    );
+  }
+}
+
+export default NewsletterGrid;
