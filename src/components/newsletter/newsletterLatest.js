19024220ff9ef0c6a7130fa1478d0a8d6b8f5174
Tanner Noble <tannernoble@gmail.com>
2018-10-15 12:34:10 -0500

    provided history prop to newsletter children components

--- src/components/newsletter/newsletterLatest.js
+++ src/components/newsletter/newsletterLatest.js
@@ -4,7 +4,7 @@ import Button from '../button';
 export default class NewsletterLatest extends Component {
 
   handleEdit = () => {
-    console.log('trying to handle edit');
+    this.props.history.push('/newsletter/edit');
   }
   
   render () {
