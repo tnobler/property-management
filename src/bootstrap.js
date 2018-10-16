109329eb7e057e508d1879f34ee89173af5f9df4
Tanner Noble <tannernoble@gmail.com>
2018-10-16 12:05:46 -0500

    built newsletter detail route and component and setup to newsletter id via req.params

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -24,6 +24,7 @@ import Signin from './components/auth/signin';
 import Dashboard from './components/dashboard';
 import NewNewsletter from './components/newsletter/newsletterNew';
 import EditNewsletter from './components/newsletter/newsletterEdit';
+import NewsletterDetail from './components/newsletter/newsletterDetail';
 
 
 function main() {
@@ -39,6 +40,7 @@ function main() {
             <Route path='/dashboard' component={requireAuth(Dashboard)}/>
             <Route path='/newsletter/new' component={requireAuth(NewNewsletter)}/>
             <Route path='/newsletter/edit/:id' component={requireAuth(EditNewsletter)}/>
+            <Route path='/newsletter/detail/:id' component={requireAuth(NewsletterDetail)}/>
           </Layout>
         </Switch>
       </Router>
---
+++ src/components/newsletter/newsletterDetail.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class NewsletterDetail extends Component {
+  render () {
+    return (
+      <div className='newsletter-detail'>
+         Newsletter Detail
+      </div>
+    );
+  }
+}
