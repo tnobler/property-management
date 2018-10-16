f6bcf74d0ca87a307335dac3d4f8408e34c5854f
Tanner Noble <tannernoble@gmail.com>
2018-10-16 11:50:10 -0500

    mapped newsletter edit data into form

--- src/components/formFields.js
+++ src/components/formFields.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 export class FormInput extends Component {
   render() {
-    const { className, title, input, type, placeholder } = this.props;
+    const { className, title, input, type, placeholder, editValue } = this.props;   
     return (
       <div className={`${className} form-input`}>
         <label className='form-input__title'>{title}</label>
@@ -10,7 +10,9 @@ export class FormInput extends Component {
           className='form-input__input'
           type={type}
           {...input}
-          placeholder={placeholder}  
+          placeholder={placeholder}
+          value={editValue ? editValue : input.value}
+          
         />
       </div>
     )
@@ -19,7 +21,7 @@ export class FormInput extends Component {
 
 export class FormTextArea extends Component {
   render() {
-    const { className, title, input, type, placeholder } = this.props;
+    const { className, title, input, type, placeholder, editValue } = this.props;   
     return (
       <div className={`${className} form-textarea`}>
         <label className='form-textarea__title'>{title}</label>
@@ -28,6 +30,7 @@ export class FormTextArea extends Component {
           type={type}
           {...input}
           placeholder={placeholder}
+          value={editValue ? editValue : ''}
         >
 
         </textarea>
@@ -65,7 +68,7 @@ export class FormImage extends Component {
   }
 
   render() {
-    const { className, title, input, type, imageUrl } = this.props;
+    const { className, title, input, imageUrl } = this.props;    
     return (
       <div className={`${className} form-image`}>
         <label className='form-image__title'>{title}</label>
