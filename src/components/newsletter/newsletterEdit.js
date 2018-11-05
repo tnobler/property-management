687d15503b0487969471ae71c85eb6e825e02d74
Tanner Noble <tannernoble@gmail.com>
2018-11-05 12:32:37 -0600

    finished editnewsletter funcionality

--- src/components/newsletter/newsletterEdit.js
+++ src/components/newsletter/newsletterEdit.js
@@ -12,11 +12,9 @@ class EditNewsletter extends Component {
     var formData = new FormData();
     formData.append('title', title);
     formData.append('body', body);
-    if(image != undefined) {
-      formData.append('image', image);
-    }
+    formData.append('image', image);
     
-    this.props.createNewNewsletter(formData, () => {
+    this.props.editNewsletter(this.props.match.params.id, formData, () => {
       this.props.history.push('/dashboard');  
     })
   };
